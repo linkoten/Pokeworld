1da453a8fc66dc9c6be53be0714568f0826b711d
linkoten <catto.francois@hotmail.fr>
2024-10-31 17:38:01 +0100

    re

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import PokemonList from "./components/Data-List";
-
 export default function Page() {
-  return (
-    <div className="p-24">
-      <PokemonList url="pokemon" />
-    </div>
-  );
+  return <div className="p-24">Yo !</div>;
 }
