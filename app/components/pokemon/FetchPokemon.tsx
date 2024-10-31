44e5ce335bc01040babf61d7d3e60da7e4c672fd
linkoten <catto.francois@hotmail.fr>
2024-10-31 17:36:41 +0100

    re

--- app/components/pokemon/FetchPokemon.tsx
+++ app/components/pokemon/FetchPokemon.tsx
@@ -1,7 +1,7 @@
 export default async function FetchPokemon({ url }: { url: string }) {
   console.log("je suis l'url", url);
-  let data = await fetch(url);
-  let posts = await data.json();
+  const data = await fetch(url);
+  const posts = await data.json();
 
   console.log(posts);
   return <div>YO</div>;
